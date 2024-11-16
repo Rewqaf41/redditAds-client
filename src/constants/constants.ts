fb2124b191ee1442c3a3b09ac5a3df913015695e
Rewqaf41 <danyburdin@gmail.com>
2024-11-16 11:24:55 +0300

    change backend url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const AUTH_URL = 'http://localhost:12345/api/v1'
-export const REDDIT_URL = 'http://localhost:3002/api/v1'
+export const AUTH_URL = 'http://46.16.36.97:12345/api/v1'
+export const REDDIT_URL = 'http://46.16.36.97:3002/api/v1'
 export const IS_CLIENT = typeof window !== 'undefined'
