504deafecd3a3bf90abed2c786754139638904f1
Rewqaf41 <danyburdin@gmail.com>
2024-12-27 22:06:16 +0300

    delete credentials

--- src/api/axiosRedditAccount.ts
+++ src/api/axiosRedditAccount.ts
@@ -1,14 +1,11 @@
 import { REDDIT_URL } from '@/constants/constants'
 import axios, { CreateAxiosDefaults } from 'axios'
 import { getContentType } from './api.helper'
-import { refreshInterceptor } from './authRefreshToken'
 
 const axiosOptions: CreateAxiosDefaults = {
 	baseURL: REDDIT_URL,
 	headers: getContentType(),
-	withCredentials: true,
+	withCredentials: false,
 }
 
 export const instance = axios.create(axiosOptions)
-
-refreshInterceptor(instance)
