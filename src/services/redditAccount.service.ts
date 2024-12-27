990fad872421251514575b8d721301d0db5d53c5
Rewqaf41 <danyburdin@gmail.com>
2024-12-27 22:29:59 +0300

    add headers - authorization

--- src/services/redditAccount.service.ts
+++ src/services/redditAccount.service.ts
@@ -1,11 +1,18 @@
 import { instance } from '@/api/axiosRedditAccount'
 import { IFormRedditAccount } from '@/types/auth.types'
+import { getAccessToken } from './auth.helper'
 
 class RedditAccount {
 	async addAccount(data: IFormRedditAccount) {
+		const token = getAccessToken()
 		const response = await instance.post(
 			`/add-account`,
-			data
+			data,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			}
 		)
 		return response
 	}
