f92a0986d2adfc75d0650765efd7793fd7f6d846
Rewqaf41 <danyburdin@gmail.com>
2025-02-22 04:05:03 +0300

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import { EnumTokens } from './services/auth/auth.service'
+
+export default async function middleware(req: NextRequest) {
+	const token = req.cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+
+	if (!token) {
+		return NextResponse.redirect(new URL("/login", req.url))
+	}
+
+}
+
+export const config = {
+	matcher: ["/", "/accounts"],
+}
