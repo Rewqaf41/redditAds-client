32c42924e63bd761bebe51e8947943f7fb242ab0
Rewqaf41 <danyburdin@gmail.com>
2025-02-22 03:59:18 +0300

    delete dev tools

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,7 +2,6 @@
 
 import LayoutClient from "@/components/layout/layout"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { PropsWithChildren, useState } from "react"
 
 export function Providers({ children }: PropsWithChildren<unknown>) {
@@ -11,7 +10,6 @@ export function Providers({ children }: PropsWithChildren<unknown>) {
 	return (
 		<QueryClientProvider client={client}>
 			<LayoutClient>{children}</LayoutClient>
-			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	)
 }
