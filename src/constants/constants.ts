9fc2f2127fd394165f7eec5fc3a12086b524f5ec
Rewqaf41 <danyburdin@gmail.com>
2025-02-22 04:09:55 +0300

    added dev and prod checkout

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,6 @@
-export const AUTH_URL = 'https://decomate.ru/api/v1'
-export const REDDIT_URL = 'https://decomate.ru/api/v1'
+export const NODE_ENV = 'production' as string
+
+export const AUTH_URL = NODE_ENV === 'dev' ? 'http://localhost:12345/api/v1' : 'https://decomate.ru/api/v1'
+export const REDDIT_URL = NODE_ENV === 'dev' ? 'http://localhost:3002/api/v1' : 'https://decomate.ru/api/v1'
+export const REDDIT_API_URL = NODE_ENV === 'dev' ? 'http://localhost:3002/api/v3' : 'https://decomate.ru/api/v3'
 export const IS_CLIENT = typeof window !== 'undefined'
