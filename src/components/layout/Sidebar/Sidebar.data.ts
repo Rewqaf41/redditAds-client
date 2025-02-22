e12ebe580448233f6d900992d06d73668587975e
Rewqaf41 <danyburdin@gmail.com>
2025-02-22 04:00:44 +0300

    new logic in sidebar

--- src/components/layout/Sidebar/Sidebar.data.ts
+++ src/components/layout/Sidebar/Sidebar.data.ts
@@ -1,13 +1,4 @@
-import { AiOutlineHome } from 'react-icons/ai'
-import { LuShare2 } from 'react-icons/lu'
-
 export const MENU = [
 	{
-		url: "/",
-		icon: AiOutlineHome
-	},
-	{
-		url: "/123",
-		icon: LuShare2
 	}
 ]
--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,28 +1,34 @@
 "use client"
+import { AddAds } from "@/components/ui/element/AddAds/AddAds"
+import { AddCamping } from "@/components/ui/element/AddCamping/AddCamping"
+import { AddGroup } from "@/components/ui/element/AddGroup/AddGroup"
+import { Hint } from "@/components/ui/element/Hint/Hint"
 import { useAuth } from "@/hooks/useAuth"
-import authService from "@/services/auth.service"
+import authService from "@/services/auth/auth.service"
+import { accountStore } from "@/store/account/account.store"
 import { useMutation } from "@tanstack/react-query"
-import cn from "clsx"
-import { Settings } from "lucide-react"
-import Link from "next/link"
-import { usePathname, useRouter } from "next/navigation"
-import { AiOutlineHome } from "react-icons/ai"
+import { BadgePlus, FolderPlus, PenBox } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 import { FcReddit } from "react-icons/fc"
-import { LuShare2 } from "react-icons/lu"
 import { TbLogout2 } from "react-icons/tb"
 import styles from "./Sidebar.module.scss"
 
 export function Sidebar() {
-	const isAuth = useAuth()
+	const [campingIsOpen, setCampingIsOpen] = useState(false)
+	const [groupIsOpen, setGroupIsOpen] = useState(false)
+	const [adsIsOpen, setAdsIsOpen] = useState(false)
+	const { selectedAccounts } = accountStore()
+	const { isAuth, exit } = useAuth()
 	const { push } = useRouter()
-	const { mutate: mutateLogout, isPending: isLogoutPending } = useMutation({
+	const { mutate: mutateLogout } = useMutation({
 		mutationKey: ["logout"],
 		mutationFn: () => authService.logout(),
 		onSuccess() {
+			exit()
 			push("/login")
 		},
 	})
-	const pathname = usePathname()
 	return (
 		<>
 			{isAuth ? (
@@ -33,29 +39,52 @@ export function Sidebar() {
 								<FcReddit size={27} className='text-white' />
 							</div>
 						</div>
-						<Link
-							href='/'
-							className={cn({
-								[styles.active]: pathname === "/",
-							})}
-						>
-							<AiOutlineHome size={27} />
-						</Link>
-						<button>
-							<LuShare2 size={27} />
-						</button>
+						{selectedAccounts.length ? (
+							<>
+								<div className='pb-5'>
+									<AddCamping
+										isOpen={campingIsOpen}
+										onClose={() => setCampingIsOpen(!campingIsOpen)}
+									/>
+									<button onClick={() => setCampingIsOpen(!campingIsOpen)}>
+										<Hint label='Создать Кампанию' side='right' asChild>
+											<BadgePlus size={27} />
+										</Hint>
+									</button>
+								</div>
+								<div className='pb-5'>
+									<AddGroup
+										isOpen={groupIsOpen}
+										onClose={() => setGroupIsOpen(!groupIsOpen)}
+									/>
+									<button onClick={() => setGroupIsOpen(!groupIsOpen)}>
+										<Hint label='Создать Группу' side='right' asChild>
+											<FolderPlus size={27} />
+										</Hint>
+									</button>
+								</div>
+								<div className='pb-5'>
+									<AddAds
+										isOpen={adsIsOpen}
+										onClose={() => setAdsIsOpen(!adsIsOpen)}
+									/>
+									<button>
+										<Hint label='Создать Рекламу' side='right' asChild>
+											<PenBox size={27} />
+										</Hint>
+									</button>
+								</div>
+							</>
+						) : null}
 					</div>
 					<div>
-						<Link href='/settings'>
-							<Settings size={27} />
-						</Link>
 						<button onClick={() => mutateLogout()}>
 							<TbLogout2 size={27} />
 						</button>
 					</div>
 				</aside>
 			) : (
-				<div className='bg-neutral-950'></div>
+				<div className='bg-[#202938]'></div>
 			)}
 		</>
 	)
