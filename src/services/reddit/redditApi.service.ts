384a1eb072d07882db96159bb25d7b72d2dc2287
Rewqaf41 <danyburdin@gmail.com>
2025-03-23 14:16:54 +0300

    new endpoint and type for him

--- src/services/reddit/redditApi.service.ts
+++ src/services/reddit/redditApi.service.ts
@@ -1,5 +1,5 @@
 import { instance } from "@/api/axiosRedditApi"
-import { IAccountInfo, IGetAccountData, IGetAccountsResponse, IRawAccountData } from '@/types/reddit/redditApi.types'
+import { IAccountData, IAccountInfo, IAdData, ICampingData, ICommonRequest, IGetAccountsResponse, IGroupData, IRawAccountData } from '@/types/reddit/redditApi.types'
 import { getAccessToken } from '../auth/auth.helper'
 
 
@@ -15,7 +15,7 @@ import { getAccessToken } from '../auth/auth.helper'
 // }
 
 class RedditApiService {
-	async getAccountsInfo(data: IGetAccountData): Promise<IGetAccountsResponse> {
+	async getAccountsInfo(data: IAccountData): Promise<IGetAccountsResponse> {
 		const token = getAccessToken()
 		const response = await instance.post(
 			"/me",
@@ -44,6 +44,93 @@ class RedditApiService {
 		})
 		return response.data
 	}
+
+	async CreateCamping(usernames: string[], data: ICampingData) {
+		const token = getAccessToken()
+		const response = await instance.post<ICommonRequest>("ПОМЕНЯТЬ ССЫЛКУ НА ПРАВИЛЬНУЮ", 
+			{
+				"usernames": usernames,
+				"rp": {
+					"name": data.name,
+					"configured_status": "ACTIVE",
+					"objective": data.objective,
+					"spend_cap": data.spend_cap
+				}
+			}, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		})
+		return response
+	}
+
+	async CreateGroup(usernames: string[], data: IGroupData) {
+		const token = getAccessToken()
+		const response = await instance.post<ICommonRequest>("ПОМЕНЯТЬ ССЫЛКУ НА ПРАВИЛЬНУЮ",
+			// TODO: ЗАПОЛНИТ ЭТУ ПОЕБОТУ😭
+			{
+				"usernames": usernames,
+				"rp": {
+					"bid_strategy": null,
+					"bid_type": null,
+					"campaign_id": "...",
+					"configured_status": "ACTIVE",
+					"end_time": data.end_time,
+					"goal_type": data.goal_type,
+					"goal_value": data.goal_value,
+					 "name": data.name,
+					 "start_time": data.start_time,
+					 "targeting": {
+						"carriers": data.carriers,
+						"communities": convertToArray(data.communities),
+						"devices": data.devices
+					 }
+				}
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			}
+	)
+	return response
+}
+
+	async CreateAds(usernames: string[] , data: IAdData) {
+		const token = getAccessToken()
+		const response = await instance.post<ICommonRequest>("ПОМЕНЯТЬ ССЫЛКУ НА ПРАВИЛЬНУЮ",
+			{
+				"usernames": usernames,
+				"rp": {
+					"type": "UNSPECIFIED",
+					"click_url": data.destination_url,
+					"configured_status": "ACTIVE",
+					"name": data.name,
+					"preview_expiry": new Date().toISOString(),
+					"shopping_creative": {
+						"allow_comments": data.allow_comments,
+						"call_to_action": data.call_to_action,
+						"destination_url": data.destination_url,
+						"headline": data.headline
+					},
+				}
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			}
+		)
+		return response
+	}
+
+}
+
+function convertToArray(data: string | undefined): string[]	| undefined {
+	if (!data) return
+	return data.split(/[,\s]+/)
+	.map((word: string) => word.trim())
+	.filter((word: string) => word.length > 0)
 }
 
 function safeParseRaw(raw: string): IRawAccountData | undefined {
--- src/types/reddit/redditApi.types.ts
+++ src/types/reddit/redditApi.types.ts
@@ -1,13 +1,49 @@
-// export interface IRedditApi {
-// 	usernames: string[]
-// 	payload?: any
-// }
-// export interface IRedditApiResponeItem {
-// 	usernames: string
-// 	raw?: IGetMeResponseBody
-// }
+export interface ICommonRequest {
+	usernames: IAccountData
+	rp: any
+}
 
-export interface IGetAccountData {
+export interface ICampingData {
+	name: string
+	objective: string
+	spend_cap: string
+}
+
+export interface IGroupData {
+	name: string
+	keywords?: string
+	communities?: string
+	interests?: string
+	geolocations?: string
+	excluded_geolocations?: string
+	gender: string | null
+	devices: string | null
+
+	ios_devices?: string[]
+	android_devices?: string[]
+	carriers?: string[]
+
+	excluded_communities?: string
+	excluded_keywords?: string
+	goal_value: string
+	goal_type: string
+	start_time: string | null
+	end_time?: string | null
+
+	campaings: string[]
+}
+
+export interface IAdData {
+  name: string
+  headline: string
+  destination_url: string
+  call_to_action: string
+  allow_comments: boolean
+
+	groups: string[]
+}
+
+export interface IAccountData {
 	usernames: string[]
 }
 
