b4458841a28417431721f18f3f54e38927dd9cea
Rewqaf41 <danyburdin@gmail.com>
2025-03-29 14:36:58 +0300

    new logic for generate metrics

--- src/utils/generateRandomMetrics.ts
+++ src/utils/generateRandomMetrics.ts
@@ -1,7 +1,10 @@
-export function generateRandomMetrics (spend?: string) {
+export function generateRandomMetrics(spend_cap?: string) {
+	const maxSpend = spend_cap ? parseFloat(spend_cap.replace(/\$/g, "").trim()) : 1000
+	const randomSpend = (Math.random() * maxSpend).toFixed(0)
+
 	return {
 		impressions: (Math.floor(Math.random() * 10000) + 5000).toString(),
-		spend: spend ? spend.toString() : (Math.floor(Math.random() * 1000) + 10).toString(),
+		spend: randomSpend,
 		clicks: (Math.floor(Math.random() * 1000) + 100).toString(),
 		ecpm: (Math.random() * 10 + 5).toFixed(2),
 		cpc: (Math.random() * 2 + 0.5).toFixed(2),
