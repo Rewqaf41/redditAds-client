48c2117bde17a8378ea1e5bc4e7c5d3a85e658c9
Rewqaf41 <danyburdin@gmail.com>
2025-03-29 14:31:01 +0300

    new button logic in sidebar

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,16 +1,17 @@
 "use client"
 import data from "@/components/ui/element/Accounts/account.json"
-import { AddAds } from "@/components/ui/element/AddAds/AddAds"
-import { AddCamping } from "@/components/ui/element/AddCamping/AddCamping"
-import { AddGroup } from "@/components/ui/element/AddGroup/AddGroup"
+import { AddAds } from "@/components/ui/element/Ads/AddAds/AddAds"
+import { AddCamping } from "@/components/ui/element/Campaings/AddCamping/AddCamping"
+import { AddGroup } from "@/components/ui/element/Group/AddGroup/AddGroup"
 import { Hint } from "@/components/ui/element/Hint/Hint"
+import { useSearchContext } from "@/components/ui/element/SearchContext"
 import { useAuth } from "@/hooks/useAuth"
 import authService from "@/services/auth/auth.service"
-import { accountStore } from "@/store/account/account.store"
 import { useMutation } from "@tanstack/react-query"
 import { BadgePlus, FolderPlus, PenBox } from "lucide-react"
-import { useRouter } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 import { useState } from "react"
+import toast from "react-hot-toast"
 import { FcReddit } from "react-icons/fc"
 import { TbLogout2 } from "react-icons/tb"
 import styles from "./Sidebar.module.scss"
@@ -19,14 +20,17 @@ export function Sidebar() {
 	const [campingIsOpen, setCampingIsOpen] = useState(false)
 	const [groupIsOpen, setGroupIsOpen] = useState(false)
 	const [adsIsOpen, setAdsIsOpen] = useState(false)
-	const { selectedItems } = accountStore()
+	const { selectedItems } = useSearchContext()
 	const { isAuth, exit } = useAuth()
 	const { push } = useRouter()
+	const pathname = usePathname()
+
 	const { mutate: mutateLogout } = useMutation({
 		mutationKey: ["logout"],
 		mutationFn: () => authService.logout(),
 		onSuccess() {
 			exit()
+			toast.success("Вы успешно вышли")
 			push("/login")
 		},
 	})
@@ -46,41 +50,43 @@ export function Sidebar() {
 								<FcReddit size={27} className='text-white' />
 							</div>
 						</div>
-						{selectedItems.length && !hasDisabledAccount ? (
+						{pathname === "/accounts" ? (
 							<>
-								<div className='pb-5'>
-									<AddCamping
-										isOpen={campingIsOpen}
-										onClose={() => setCampingIsOpen(!campingIsOpen)}
-									/>
-									<button onClick={() => setCampingIsOpen(!campingIsOpen)}>
-										<Hint label='Создать Кампанию' side='right' asChild>
-											<BadgePlus size={27} />
-										</Hint>
-									</button>
-								</div>
-								<div className='pb-5'>
-									<AddGroup
-										isOpen={groupIsOpen}
-										onClose={() => setGroupIsOpen(!groupIsOpen)}
-									/>
-									<button onClick={() => setGroupIsOpen(!groupIsOpen)}>
-										<Hint label='Создать Группу' side='right' asChild>
-											<FolderPlus size={27} />
-										</Hint>
-									</button>
-								</div>
-								<div className='pb-5'>
-									<AddAds
-										isOpen={adsIsOpen}
-										onClose={() => setAdsIsOpen(!adsIsOpen)}
-									/>
-									<button onClick={() => setAdsIsOpen(!adsIsOpen)}>
-										<Hint label='Создать Рекламу' side='right' asChild>
-											<PenBox size={27} />
-										</Hint>
-									</button>
-								</div>
+								<AddCamping
+									isOpen={campingIsOpen}
+									onClose={() => setCampingIsOpen(!campingIsOpen)}
+								/>
+								<button onClick={() => setCampingIsOpen(!campingIsOpen)}>
+									<Hint label='Создать Кампанию' side='right' asChild>
+										<BadgePlus size={27} />
+									</Hint>
+								</button>
+							</>
+						) : null}
+						{pathname === "/campaings" ? (
+							<>
+								<AddGroup
+									isOpen={groupIsOpen}
+									onClose={() => setGroupIsOpen(!groupIsOpen)}
+								/>
+								<button onClick={() => setGroupIsOpen(!groupIsOpen)}>
+									<Hint label='Создать Группу' side='right' asChild>
+										<FolderPlus size={27} />
+									</Hint>
+								</button>
+							</>
+						) : null}
+						{pathname === "/groups" ? (
+							<>
+								<AddAds
+									isOpen={adsIsOpen}
+									onClose={() => setAdsIsOpen(!adsIsOpen)}
+								/>
+								<button onClick={() => setAdsIsOpen(!adsIsOpen)}>
+									<Hint label='Создать Рекламу' side='right' asChild>
+										<PenBox size={27} />
+									</Hint>
+								</button>
 							</>
 						) : null}
 					</div>
