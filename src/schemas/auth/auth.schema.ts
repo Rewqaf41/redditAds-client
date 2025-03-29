eddb6b63e5945cb3d5ec9f308ee0b85cfcd9c9e2
Rewqaf41 <danyburdin@gmail.com>
2025-03-29 14:37:46 +0300

    new validate for auth

--- src/schemas/auth/auth.schema.ts
+++ src/schemas/auth/auth.schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const loginSchema = z.object({
-	username: z.string().min(1).regex(/^[a-zA-Z0-9_]+$/, "Логин может содержать только латинские буквы, цифры и _"),
+	username: z.string().min(1, "Поле должно содержать хотя бы 1 символ").regex(/^[a-zA-Z0-9_]+$/, "Логин может содержать только латинские буквы, цифры и _"),
 	password: z.string(),
 })
 
