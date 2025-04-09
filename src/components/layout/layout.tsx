0fb989bcc719972493ad17bff12e4e54d04db805
Rewqaf41 <danyburdin@gmail.com>
2025-04-10 00:11:44 +0300

    added unselect element before switching to another page

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,19 +1,25 @@
 import { AreaChart } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import type { PropsWithChildren } from "react"
+import { useEffect, type PropsWithChildren } from "react"
 import { useSearchContext } from "../ui/element/SearchContext"
 import { StatisticButton } from "../ui/element/Statistic/StatisticButton/StatisticButton"
 import styles from "./layout.module.scss"
 import { Sidebar } from "./Sidebar/Sidebar"
 
 export default function LayoutClient({ children }: PropsWithChildren<unknown>) {
-	const { selectedItems } = useSearchContext()
+	const { selectedItems, unselectAll } = useSearchContext()
 	const pathname = usePathname()
+
+	useEffect(() => {
+		if (pathname === "/statistics") return
+		unselectAll()
+	}, [pathname, unselectAll])
+
 	return (
 		<main className={styles.layout}>
 			<Sidebar />
-			<section className={styles.content}>{children}</section>
+			<section>{children}</section>
 			{selectedItems.length > 0 && pathname !== "/statistics" && (
 				<Link href='/statistics' className='absolute bottom-5 right-5'>
 					<StatisticButton icon={AreaChart} />
