dd774cd281ac7a158e82b4067048e009e4d0c6dc
Rewqaf41 <danyburdin@gmail.com>
2025-04-10 00:14:13 +0300

    fixed the double scrollbar

--- src/components/ui/element/Statistic/Statistic.tsx
+++ src/components/ui/element/Statistic/Statistic.tsx
@@ -6,15 +6,12 @@ import { HorizontalBarChart } from "./Charts/HorizontalBar"
 export function Statistic() {
 	return (
 		<>
-			<div className='bg-[#101727] h-14 flex items-center p-5 sticky top-0'>
-				Статистика
-			</div>
-			<div className='h-full w-full overflow-y-auto'>
+			<div className='h-screen w-full'>
 				<div className='flex flex-col items-center justify-center'>
 					<HorizontalBarChart />
 				</div>
-				<div className='mt-2 flex border-t-2 h-screen border-border'>
-					<div className='mt-4 '>
+				<div className='mt-2 flex border-t-2 border-border'>
+					<div className='mt-4'>
 						<DoughnutChart />
 					</div>
 				</div>
